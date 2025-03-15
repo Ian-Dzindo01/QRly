e64fe3158b3f3a899bd1fb2b87ac52dbfe090983
Ian-Dzindo01 <ian.dzindo01@gmail.com>
2025-03-15 17:08:44 +0100

    Decoders

--- QRly/Program.cs
+++ QRly/Program.cs
@@ -1,11 +1,40 @@
 ﻿using QRly.Encoder;
 using QRly.Helpers;
 
-string input = "222222222";
+string input = "222333";
 QRMode mode = QRHelper.DetermineMode(input);
 List<string> encodedData = Encoder.EncodeQRCodeData(input, mode);
 
-foreach (var bitString in encodedData)
+Console.WriteLine(encodedData[2]);
+Console.WriteLine(DecodeNumeric(encodedData[2]));
+
+
+static string DecodeNumeric(string input)
 {
-    Console.WriteLine(bitString);
+    string result = "";
+    int i = 0;
+
+    while (i < input.Length)
+    {
+        // Determine the length of the binary chunk
+        int bitLength;
+        if (input.Length - i >= 10)
+            bitLength = 10;  // 3 digits encoded in 10 bits
+        else if (input.Length - i >= 7)
+            bitLength = 7;   // 2 digits encoded in 7 bits
+        else
+            bitLength = 4;   // 1 digit encoded in 4 bits
+
+        // Get the binary chunk and convert it to a number
+        string binChunk = input.Substring(i, bitLength);
+        int number = Convert.ToInt32(binChunk, 2);
+
+        // Add the decoded number to the result
+        result += number.ToString();
+
+        // Move to the next chunk
+        i += bitLength;
+    }
+
+    return result;
 }
