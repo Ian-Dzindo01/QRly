51198b39cc902f97a440c748cde353ff359cc433
Ian-Dzindo01 <ian.dzindo01@gmail.com>
2025-03-16 10:27:17 +0100

    More decoders

---
+++ QRly/Decoder.cs
@@ -0,0 +1,73 @@
+﻿using System.Text;
+
+namespace QRly.Decoder
+{
+    public static class Decoder
+    {
+        static string DecodeNumeric(string input)
+        {
+            string result = "";
+            int i = 0;
+
+            while (i < input.Length)
+            {
+                int bitLength;
+                if (input.Length - i >= 10)
+                    bitLength = 10;
+                else if (input.Length - i >= 7)
+                    bitLength = 7;
+                else
+                    bitLength = 4;
+
+                string binChunk = input.Substring(i, bitLength);
+                int number = Convert.ToInt32(binChunk, 2);
+
+                result += number.ToString();
+
+                i += bitLength;
+            }
+
+            return result;
+        }
+
+        public static string DecodeAlphanumeric(string bitString)
+        {
+            const string ALPHANUMERIC_CHARS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ $%*+-./:";
+            string result = "";
+
+            for (int i = 0; i < bitString.Length;)
+            {
+                if (bitString.Length - i >= 11)
+                {
+                    int value = Convert.ToInt32(bitString.Substring(i, 11), 2);
+                    int first = value / 45;
+                    int second = value % 45;
+                    result += ALPHANUMERIC_CHARS[first].ToString() + ALPHANUMERIC_CHARS[second].ToString();
+                    i += 11;
+                }
+                else if (bitString.Length - i >= 6)
+                {
+                    int value = Convert.ToInt32(bitString.Substring(i, 6), 2);
+                    result += ALPHANUMERIC_CHARS[value].ToString();
+                    i += 6;
+                }
+            }
+
+            return result;
+        }
+
+        public static string DecodeByte(string bitString)
+        {
+            StringBuilder result = new StringBuilder();
+
+            for (int i = 0; i < bitString.Length; i += 8)
+            {
+                string byteSegment = bitString.Substring(i, 8);
+                int charCode = Convert.ToInt32(byteSegment, 2);
+                result.Append((char)charCode);
+            }
+
+            return result.ToString();
+        }
+    }
+}
--- QRly/Program.cs
+++ QRly/Program.cs
@@ -1,40 +1,16 @@
-﻿using QRly.Encoder;
+﻿using QRly.Decoder;
+using QRly.Encoder;
 using QRly.Helpers;
 
-string input = "222333";
+string input = "SAMIRMANGA #2234";
 QRMode mode = QRHelper.DetermineMode(input);
 List<string> encodedData = Encoder.EncodeQRCodeData(input, mode);
 
 Console.WriteLine(encodedData[2]);
-Console.WriteLine(DecodeNumeric(encodedData[2]));
+Console.WriteLine(Decoder.DecodeByte(encodedData[2]));
 
 
-static string DecodeNumeric(string input)
-{
-    string result = "";
-    int i = 0;
-
-    while (i < input.Length)
-    {
-        // Determine the length of the binary chunk
-        int bitLength;
-        if (input.Length - i >= 10)
-            bitLength = 10;  // 3 digits encoded in 10 bits
-        else if (input.Length - i >= 7)
-            bitLength = 7;   // 2 digits encoded in 7 bits
-        else
-            bitLength = 4;   // 1 digit encoded in 4 bits
-
-        // Get the binary chunk and convert it to a number
-        string binChunk = input.Substring(i, bitLength);
-        int number = Convert.ToInt32(binChunk, 2);
-
-        // Add the decoded number to the result
-        result += number.ToString();
-
-        // Move to the next chunk
-        i += bitLength;
-    }
-
-    return result;
-}
+//foreach (string bitString in encodedData)
+//{
+//    Console.WriteLine(bitString);
+//}
